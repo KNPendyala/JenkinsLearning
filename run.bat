b0271e0c9e49ac8ab859902b7b93e5aab4c35028
KNPendyala <K.N.Pendyala@gmail.com>
2018-01-13 17:05:29 -0500

    Project added.

---
+++ run.bat
@@ -0,0 +1,5 @@
+set classpath=C:\Users\Kumar\eclipse-workspace\Jenkins\bin
+
+set classpath=%classpath%;C:\Users\Kumar\eclipse-workspace\Jenkins\lib\*
+
+java org.testng.TestNG testng.xml
---
+++ src/JenkinsLearn/JenkinsLearning.java
@@ -0,0 +1,21 @@
+package JenkinsLearn;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class  JenkinsLearning {
+	
+	@Test
+	public void jenkinsDemo() {
+		System.out.println("Hello Jenkins");
+		
+		WebDriver driver;
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Kumar\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		driver= new ChromeDriver();
+		driver.get("http://www.bcbsm.com"); 
+		System.out.println(driver.getTitle());
+		driver.quit();
+		
+	}
+}
