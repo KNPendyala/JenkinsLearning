8e993c5a3d8f04e494c8cd1f761c17c0b9431353
KNPendyala <K.N.Pendyala@gmail.com>
2018-01-13 18:56:07 -0500

    updated

--- src/JenkinsLearn/JenkinsLearning.java
+++ src/JenkinsLearn/JenkinsLearning.java
@@ -8,11 +8,7 @@ public class  JenkinsLearning {
 	
 	@Test
 	public void jenkinsDemo() {
-<<<<<<< HEAD
 		System.out.println("Hello, Welcome Jenkins..");
-=======
-		System.out.println("Hello Jenkins");
->>>>>>> parent of 60c7420... Project modified
 		
 		WebDriver driver;
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Kumar\\Downloads\\chromedriver_win32\\chromedriver.exe");
