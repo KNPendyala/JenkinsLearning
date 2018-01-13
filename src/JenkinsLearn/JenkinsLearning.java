60c7420bf361464f9fecb36714603444f4010dc0
KNPendyala <K.N.Pendyala@gmail.com>
2018-01-13 17:07:10 -0500

    Project modified

--- src/JenkinsLearn/JenkinsLearning.java
+++ src/JenkinsLearn/JenkinsLearning.java
@@ -8,7 +8,7 @@ public class  JenkinsLearning {
 	
 	@Test
 	public void jenkinsDemo() {
-		System.out.println("Hello Jenkins");
+		System.out.println("Hello Jenkins..");
 		
 		WebDriver driver;
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Kumar\\Downloads\\chromedriver_win32\\chromedriver.exe");
